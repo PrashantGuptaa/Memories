8b1eeb55d66296143232ecc51adfe1e222bc526c
PrashantGuptaa <meengineerprashant@gmail.com>
2022-02-13 14:35:42 +0530

    Code Refactor

--- db/Schema/memories.sql
+++ db/Schema/memories.sql
@@ -3,5 +3,6 @@ id serial not null primary key unique,
 user_id int4 not null,
 "name" varchar,
 description varchar,
+created TIMESTAMPTZ DEFAULT NOW(),
 foreign key(user_id) references public.users(id) on delete cascade
 );
