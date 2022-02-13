fb8429220cc85593ba567e2da0f2ced4728af383
PrashantGuptaa <meengineerprashant@gmail.com>
2022-02-13 13:41:13 +0530

    Route Fix

--- Routes/memories.js
+++ Routes/memories.js
@@ -2,10 +2,10 @@ const express = require('express');
 const memoriesRoutes = express.Router();
 
 memoriesRoutes.post('/create/:userId', (re, res) => {res.send("TEST RESPONSE")}) // Create Memory
-memoriesRoutes.get('/edit/:memoryId/:userId', () => {}) // Edit Memory
-memoriesRoutes.get('/add/images/:memoryId/:userId', () => {}) // Add Images to a memory
-memoriesRoutes.get('/delete/images/:memoryId/:userId', () => {}) // Delete Image from a memory
-memoriesRoutes.get('/delete/memory/:memoryId/:userId', () => {}) // Delete Complete Memory
+memoriesRoutes.patch('/edit/:memoryId/:userId', () => {}) // Edit Memory
+memoriesRoutes.patch('/add/images/:memoryId/:userId', () => {}) // Add Images to a memory
+memoriesRoutes.delete('/delete/images/:memoryId/:userId', () => {}) // Delete Image from a memory
+memoriesRoutes.delete('/delete/memory/:memoryId/:userId', () => {}) // Delete Complete Memory
 memoriesRoutes.get('/view/allMemories/:userId/:otherPersonEmailId', () => {}) // View Other User All Memories
 memoriesRoutes.get('/view/memory/:userId/:otherPersonEmailId/:memoryId', () => {}) // View Other User Specific Memory
 memoriesRoutes.get('/view/allMyMemories/:userId', () => {}) // View  My All Memories
